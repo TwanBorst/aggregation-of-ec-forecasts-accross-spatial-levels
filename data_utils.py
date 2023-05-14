c844147fd4ef62d933a1e6c87d336a4d8fb30d93
Twan Borst <me@twanborst.com>
2023-05-15 00:25:58 +0200

    Add function to generate metadata from data

--- data_utils.py
+++ data_utils.py
@@ -1,9 +1,9 @@
 from typing import List
 import numpy as np
 import pandas as pd
-from dask.distributed import Client
 import dask.dataframe as dd
-from sensor_type import SensorType, SENSOR_NAMES
+from sensor_type import SENSOR_NAMES
+from math import ceil, floor
 
 def availability_converter(val : str):
     return 0 if val == '' else int(val.strip('%'))
@@ -12,6 +12,9 @@ def sensor_converter(val : str):
     return True if val == "yes" else False
 
 metadata_dtype = {'dataid': int, 'active_record': bool, 'building_type': str, 'city': str, 'state': str, 'egauge_1s_min_time': object, 'egauge_1s_max_time': object, 'egauge_1min_min_time': object, 'egauge_1min_max_time': object}
+data_dtype = {sensor : float for sensor in SENSOR_NAMES}
+data_dtype['dataid'] = int
+data_dtype['localminute'] = object
 
 metadata_converters = {'egauge_1min_data_availability': availability_converter, 'egauge_1s_data_availability': availability_converter}
 metadata_converters.update({sensor: sensor_converter for sensor in SENSOR_NAMES})
@@ -94,3 +97,57 @@ def get_data_ids_from_metadata(data_path: str = '../data/*/metadata.csv', requir
         ddf = ddf[ddf['egauge_1min_max_time'] >= str(max_1min_time)]
     
     return ddf['dataid'].compute().to_list()
+
+def generate_metadata(data_path: str, save_path: str, metadata_files: str = None, extra_metadata_cols : List[str] = ['city', 'state']):
+    """
+    Generates the correct metadata for all the 1-second csv data files found with globstring `data_path`. If the complete metadata is provided,
+    the columns listed in `extra_metadata_cols` will be added to the generated metadata.
+
+    Parameters:
+    -----------
+    data_path : str
+        Globstring for data files.
+    save_path : str
+        Where to save the generated metadata.
+    metadata_files : str, default None
+        Globstring for complete metadata files.
+    extra_metadata_cols : List[str], default ['city', 'state']
+        Extra columns from complete metadata to be added to the generated metadata.
+    """
+    all_present = dd.Aggregation('all_present',
+                                 chunk=lambda x: x.aggregate(lambda x: x.notna().any()),
+                                 agg=lambda x: x.aggregate(lambda x: x.any()),
+                                 finalize=lambda x: x.replace({True: "yes", False: ""}))
+
+    collumns = ['dataid', 'localminute'] + SENSOR_NAMES
+    agg_meta = {'egauge_1s_min_time': 'object', 'egauge_1s_max_time': 'object', 'egauge_1s_data_availability': 'int'}
+    agg_meta.update({sensor: 'str' for sensor in SENSOR_NAMES})
+    
+    ddf : dd.DataFrame = dd.read_csv(data_path, dtype = data_dtype, blocksize=10e7, usecols=collumns)
+    
+    reorder_collumns = list(ddf.columns)
+    reorder_collumns.insert(1, 'aggregation_abs_error')
+    ddf = ddf.assign(aggregation_abs_error=(ddf[[x for x in SENSOR_NAMES if x not in ["grid", "solar", "solar2"]]].sum(axis=1) - ddf[["solar", "solar2", "grid"]].sum(axis=1)).abs().where(cond=ddf["grid"].notnull(), other=np.nan))[reorder_collumns]
+
+    agg_dict = {'localminute': {'egauge_1s_min_time': 'min', 'egauge_1s_max_time': 'max', 'egauge_1s_data_availability': 'count'}, 'aggregation_abs_error': {'aggregation_abs_error_max' : 'max', 'aggregation_abs_error_mean': 'mean'}}
+    agg_dict.update({sensor: {sensor: all_present} for sensor in SENSOR_NAMES})
+    
+    ddf = ddf.groupby('dataid', sort=False).agg(agg_dict)
+    ddf.columns = ddf.columns.get_level_values(1)
+    
+    ddf['egauge_1s_min_time'] = dd.to_datetime(ddf['egauge_1s_min_time'], utc=True)
+    ddf['egauge_1s_max_time'] = dd.to_datetime(ddf['egauge_1s_max_time'], utc=True)
+    ddf['egauge_1s_data_availability'] = (ddf['egauge_1s_data_availability'] / ((ddf['egauge_1s_max_time'] - ddf['egauge_1s_min_time'] + np.timedelta64(1, "s")) / np.timedelta64(1, "s"))).apply(lambda x: str(floor(x*10000)/100)+"%", meta=('egauge_1s_data_availability', str))
+    ddf['aggregation_abs_error_max'] = ddf['aggregation_abs_error_max'].apply(lambda x: str(floor(x*100)/100), meta=('aggregation_abs_error_max', str))
+    ddf['aggregation_abs_error_mean'] = ddf['aggregation_abs_error_mean'].apply(lambda x: str(floor(x*100)/100), meta=('aggregation_abs_error_mean', str))
+    
+    merged = ddf
+    if metadata_files is not None:
+        merged_columns = extra_metadata_cols + list(ddf.columns)[0:]
+
+        original_metadata : dd.Dataframe = dd.read_csv(metadata_files, skiprows=[1], dtype = metadata_dtype, blocksize=10e7, usecols=['dataid']+extra_metadata_cols)
+        original_metadata = original_metadata.set_index('dataid')
+        
+        merged = ddf.merge(original_metadata, how='left', left_index=True, right_index=True)[merged_columns]
+    
+    merged.to_csv(save_path, single_file = True)
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pandas as pd
 from dask.distributed import Client
-from data_utils import get_data_ids_from_metadata, process_data
+from data_utils import get_data_ids_from_metadata, process_data, generate_metadata
 
 if __name__ == '__main__':
-    metadata_glob='../data/1s_data_*/metadata.csv'
-    data_glob='../data/1s_data_*/1s_data_*.csv'
-    save_glob='../data/processed/1s_data_partition_*.csv'
+    metadata_glob='../data/metadata.csv'
+    data_glob='../data/1s_data*/1s_data*.csv'
+    save_glob='../data/processed/metadata_austin_file1_*.csv'
+    save_metadata_path='../data/processed/metadata.csv'
+    
+    # data_glob='../data/test.csv'
 
     from_timestamp = pd.Timestamp('2018-06-01 00:00:00-00:00')
     end_timestamp = pd.Timestamp('2019-07-01 00:00:00-00:00')
 
-    client = Client(n_workers=8, threads_per_worker=2, memory_limit="15GB")
+    client = Client(n_workers=6, threads_per_worker=2, memory_limit="6GB")
     print(client)
 
-    data_ids = get_data_ids_from_metadata(data_path=metadata_glob, min_1s_completeness=100, min_1s_time=from_timestamp, max_1s_time=end_timestamp)
-    process_data(files=data_glob, save_path=save_glob, sensors=[], data_ids=data_ids, from_time=from_timestamp, end_time=end_timestamp)
+    # data_ids = get_data_ids_from_metadata(data_path=metadata_glob, min_1s_completeness=100, min_1s_time=from_timestamp, max_1s_time=end_timestamp)
+    # process_data(files=data_glob, save_path=save_glob, sensors=[], data_ids=data_ids, from_time=from_timestamp, end_time=end_timestamp)
+    generate_metadata(data_path=data_glob, metadata_files=metadata_glob, save_path=save_metadata_path)
 
 
