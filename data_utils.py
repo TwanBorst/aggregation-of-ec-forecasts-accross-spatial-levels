0915d06bfe4d59cc030ec56e7bb8668cb7eaa457
Twan Borst <me@twanborst.com>
2023-05-22 10:26:22 +0200

    Add data processing function
    The function will fill missing rows, filter outliers.

--- data_utils.py
+++ data_utils.py
@@ -3,7 +3,9 @@ import numpy as np
 import pandas as pd
 import dask.dataframe as dd
 from sensor_type import SENSOR_NAMES
-from math import ceil, floor
+from math import floor
+import pyarrow as pa
+import tempfile
 
 def availability_converter(val : str):
     return 0 if val == '' else int(val.strip('%'))
@@ -19,25 +21,100 @@ data_dtype['localminute'] = object
 metadata_converters = {'egauge_1min_data_availability': availability_converter, 'egauge_1s_data_availability': availability_converter}
 metadata_converters.update({sensor: sensor_converter for sensor in SENSOR_NAMES})
 
-def process_data(files: str, save_path: str, sensors: List[str] = None, data_ids: List[int] = None, from_time: pd.Timestamp = None, end_time: pd.Timestamp = None):
-    
-    collumns = ['dataid', 'localminute'] + (sensors if sensors is not None else SENSOR_NAMES)
-    data_dtype = {sensor : float for sensor in (sensors if sensors is not None else SENSOR_NAMES)}
+def process_data(files: str, metadata_files:str, save_path: str, dumpsite: str, from_time: pd.Timestamp, end_time: pd.Timestamp, sensors: List[str] = None):
+    r"""
+    Used to process the original unpacked data. Missing rows (seconds) are filled with the last value of the 5 seconds that came before.
+    If there is no pre-existing value within those 5 seconds, the value will be set to 0.
+    Additionally values that represent over 1MW are removed and treated as if they were empty from the start.
+       
+
+    Paramters
+    ---------
+    files : `str`
+        The globstring ("path/to/files/*.csv") to the orignal unpacked csv files
+    metadata_files : `str`
+        The path to the metadata file made by `generate_metadata`
+    save_path : `str`
+        The first location that can be used for storing intermediate results
+    dumpsite : `str`
+        The second location that can be used for storing intermediate results. Additionally it will also be used to store the final result
+    from_time : `pd.Timestamp`
+        Time after which you want the 1-second data to start
+    end_time : `pd.Timestamp`
+        Time after which you want the 1-second data to end
+    sensors : `List[str]`, default `None`
+        List of sensors that will be in the final result. If the value is `None` all sensors are included.
+    """
+    sensors_excluding_grid = (sensors if sensors is not None else [sensor for sensor in SENSOR_NAMES if sensor != "grid"])
+    collumns = ['dataid', 'localminute'] + sensors_excluding_grid
+    data_dtype = {sensor : float for sensor in sensors_excluding_grid}
     data_dtype['dataid'] = int
     data_dtype['localminute'] = object
     
-    ddf = dd.read_csv(files, dtype = data_dtype, blocksize=10e7, usecols=collumns)
-    if data_ids is not None:
-        ddf = ddf[ddf['dataid'].isin(data_ids)]
+    ddf : dd.DataFrame = dd.read_csv(files, dtype = data_dtype, blocksize=10e7, usecols=collumns)
+    original_metadata : dd.DataFrame = dd.read_csv(metadata_files, dtype = metadata_dtype, blocksize=10e7, usecols=['dataid', 'state'])
     
-    if from_time is not None or end_time is not None:
-        ddf['localminute'].map_partitions(pd.to_datetime, utc=True, meta={'localminute': 'datetime64[s]'})
-        if from_time is not None:
-            ddf = ddf[ddf['localminute'] >= str(from_time)]
-        if end_time is not None:
-            ddf = ddf[ddf['localminute'] <= str(end_time)]
+    # Pretend the data from Texas was recorded one year later
+    texas_dataids: List[int] = original_metadata[original_metadata['state']=='Texas']['dataid'].compute().values.tolist()
+    ddf['localminute'] = dd.to_datetime(ddf['localminute'], utc=True)
+    ddf['localminute'] = ddf['localminute'].mask(ddf['dataid'].isin(texas_dataids), ddf['localminute']+np.timedelta64(1, 'Y'))
+        
+    dd.to_parquet(ddf.reset_index(), save_path+"/time-adjusted", write_index=False, partition_on=["dataid"], name_function=lambda x: f"data-{x}.parquet", schema={'localminute': pa.timestamp(unit='s', tz='UTC'), 'dataid': pa.int32()}, overwrite=True)
+    dataids: List[int] = original_metadata['dataid'].compute().values.tolist()
+    del original_metadata
     
-    ddf.to_csv(save_path)
+    date_range = dd.from_pandas(pd.date_range(start=from_time, end=end_time, freq='S').to_frame(name='localminute'), sort=False, npartitions=5)
+    dd.to_parquet(date_range, dumpsite+"/date-range", write_index=False, name_function=lambda x: f"data-{x}.parquet", schema={'localminute': pa.timestamp(unit='s', tz='UTC')}, overwrite=True)
+    del date_range
+    
+    total = len(dataids)
+    
+    for progress, dataid in enumerate(dataids):
+        print("\n-------------------------------", f"|     Starting with dataid={dataid}.  progress: {progress}/{total}    |", "--------------------------------\n")
+        
+        dataid_ddf = dd.read_parquet(save_path+"/time-adjusted",
+                                     filters=[('dataid', '==', dataid)],
+                                     columns=sensors_excluding_grid+['localminute'])
+        date_range = dd.read_parquet(dumpsite+"/date-range")
+        merged = date_range.merge(dataid_ddf, how='left', on=["localminute"]).assign(dataid=dataid)
+        dd.to_parquet(merged, dumpsite+"/rows-filled",
+                      write_index=False, partition_on=["dataid"],
+                      name_function=lambda x: f"data-{x}.parquet",
+                      schema={'localminute': pa.timestamp(unit='s', tz='UTC'), 'dataid': pa.int32()},
+                      append=True)
+        
+        print("\n--------------------------------", f"|      Done with dataid={dataid}       |", "--------------------------------\n")
+                                           
+    
+    ddf: dd.DataFrame = dd.read_parquet(dumpsite+"/rows-filled")
+            
+    # If a sensor reports 1MW of charging we interperet it as an error and remove the datapoint
+    # Fill in small gaps of 5 seconds with the previously encountered value and fill the rest up with 0's
+    for progress, dataid in enumerate(dataids):
+        print("\n-------------------------------", f"|     Starting with dataid={dataid}.  progress: {progress}/{total}    |", "--------------------------------\n")
+        
+        dataid_ddf = dd.read_parquet(dumpsite+"/rows-filled",
+                                     filters=[('dataid', '==', dataid)],
+                                     columns=sensors_excluding_grid+["dataid"], index=["localminute"])
+        dataid_ddf[sensors_excluding_grid] = dataid_ddf[sensors_excluding_grid].mask(dataid_ddf[sensors_excluding_grid] > 1000, np.nan).fillna(method="ffill", limit=5).fillna(0)
+        
+        dd.to_parquet(dataid_ddf.reset_index(), dumpsite+"/final",
+                      write_index=False, partition_on=["dataid"],
+                      name_function=lambda x: f"data-{x}.parquet",
+                      schema={'localminute': pa.timestamp(unit='s', tz='UTC'), 'dataid': pa.int32()},
+                      append=True)
+        
+        print("\n--------------------------------", f"|      Done with dataid={dataid}       |", "--------------------------------\n")
+    
+def merge_by_id(ddf : dd.DataFrame, date_range_path):
+    date_range = dd.read_parquet(date_range_path)
+    return date_range.merge(ddf, how='left', on=['localminute'])
+
+def persist_to_disk(location: str, dataframe: dd.DataFrame) -> dd.DataFrame:
+    dd.to_parquet(dataframe.reset_index(), location, write_index=False, partition_on=["dataid"], name_function=lambda x: f"data-{x}.parquet", schema={'localminute': pa.timestamp(unit='s', tz='UTC'), 'dataid': pa.int32()}, overwrite=True)
+    ddf = dd.read_parquet(location)
+    del ddf['index']
+    return ddf
 
 def get_data_ids_from_metadata(data_path: str = '../data/*/metadata.csv', required_sensor_data : List[str] = [],
                      min_1s_completeness: int = 0, min_1min_completeness: int = 0,
@@ -127,7 +204,7 @@ def generate_metadata(data_path: str, save_path: str, metadata_files: str = None
     
     reorder_collumns = list(ddf.columns)
     reorder_collumns.insert(1, 'aggregation_abs_error')
-    ddf = ddf.assign(aggregation_abs_error=(ddf[[x for x in SENSOR_NAMES if x not in ["grid", "solar", "solar2"]]].sum(axis=1) - ddf[["solar", "solar2", "grid"]].sum(axis=1)).abs().where(cond=ddf["grid"].notnull(), other=np.nan))[reorder_collumns]
+    ddf = ddf.assign(aggregation_abs_error=(ddf[[x for x in SENSOR_NAMES if x not in ["grid", "solar", "solar2", "battery1"]]].sum(axis=1) - ddf[["solar", "solar2", "battery1", "grid"]].sum(axis=1)).abs().where(cond=ddf["grid"].notnull(), other=np.nan))[reorder_collumns]
 
     agg_dict = {'localminute': {'egauge_1s_min_time': 'min', 'egauge_1s_max_time': 'max', 'egauge_1s_data_availability': 'count'}, 'aggregation_abs_error': {'aggregation_abs_error_max' : 'max', 'aggregation_abs_error_mean': 'mean'}}
     agg_dict.update({sensor: {sensor: all_present} for sensor in SENSOR_NAMES})
--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from dask.distributed import Client
 from data_utils import get_data_ids_from_metadata, process_data, generate_metadata
 
 if __name__ == '__main__':
-    metadata_glob='../data/metadata.csv'
+    original_metadata='../data/metadata.csv'
+    generated_metadata='../data/generated/metadata.csv'
     data_glob='../data/1s_data*/1s_data*.csv'
-    save_glob='../data/processed/metadata_austin_file1_*.csv'
+    save_dir='../data/generated/'
     save_metadata_path='../data/processed/metadata.csv'
     
-    # data_glob='../data/test.csv'
+    from_timestamp = pd.Timestamp('2019-05-01 05:00:00+00:00', tz="UTC")
+    end_timestamp = pd.Timestamp('2019-11-01 04:59:59+00:00', tz="UTC")
 
-    from_timestamp = pd.Timestamp('2018-06-01 00:00:00-00:00')
-    end_timestamp = pd.Timestamp('2019-07-01 00:00:00-00:00')
-
-    client = Client(n_workers=6, threads_per_worker=2, memory_limit="6GB")
+    client = Client(n_workers=4, threads_per_worker=2, memory_limit="6GB")
     print(client)
-
-    # data_ids = get_data_ids_from_metadata(data_path=metadata_glob, min_1s_completeness=100, min_1s_time=from_timestamp, max_1s_time=end_timestamp)
-    # process_data(files=data_glob, save_path=save_glob, sensors=[], data_ids=data_ids, from_time=from_timestamp, end_time=end_timestamp)
-    generate_metadata(data_path=data_glob, metadata_files=metadata_glob, save_path=save_metadata_path)
+ 
+    # Uncomment for generating metadata for available data
+    # generate_metadata(data_path=generated_metadata, metadata_files=original_metadata, save_path=save_metadata_path)
+    
+    # Uncomment for processing all the data (includes cleaning) and storing it on disk
+    process_data(files=data_glob, metadata_files=generated_metadata, save_path=save_dir, from_time=from_timestamp, end_time=end_timestamp, dumpsite=save_dir)
+    
 
 
